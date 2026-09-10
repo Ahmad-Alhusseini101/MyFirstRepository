8fd4ecf7b03c74ebe3f80db11da442aa89e384a3
Ahmad Al-Hosseini <ahmadalhosseini101@gmail.com>
2026-09-10 15:33:34 +0800

    something

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-print(f"{123443.43123:>20.2f}")
+age = input ("Enter your age (enter q to exit): ")
+if age == "q" or age == "Q":
+    print("Exiting the program.")
+    exit()
+
+
+while age == "":
+    print("Age cannot be empty. Please enter your age.")
+    age = input("Enter your age (enter q to exit): ")
+    if age == "q":
+        print("Exiting the program.")
+        exit()
+if age.isalpha():
+    print("Invalid input. Please enter a valid age.")
+elif round(float(age)) < 18:
+    print("You are a minor.")
+else:
+    print("You are an adult.")
