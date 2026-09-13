f9dca86a6fca52209d8aa577d3002a860e7a9ede
Ahmad Al-Hosseini <ahmadalhosseini101@gmail.com>
2026-09-13 20:15:33 +0800

    something

--- test.py
+++ test.py
@@ -1,16 +1,3 @@
-age = input ("Enter your age (enter q to exit): ")
-if age == "q" or age == "Q":
-    print("Exiting the program.")
-    exit()
-
-
-while age == "":
-    print("Age cannot be empty. Please enter your age.")
-    age = input("Enter your age (enter q to exit): ")
-    if age == "q":
-        print("Exiting the program.")
-        exit()
-if age.isalpha():
-    print("Invalid input. Please enter a valid age.")
-elif round(float(age)) < 18:
-    print("You are a minor.")
+Food = {"apple": 5, "banana": 3, "orange": 8}
+for x in Food:
+    print(x, Food[x])
