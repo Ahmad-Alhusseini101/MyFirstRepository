ffe1a6507a04a7f3ede8d0bd79df1e571f8465b7
Ahmad Al-Hosseini <ahmadalhosseini101@gmail.com>
2026-09-13 21:44:12 +0800

    something

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-Food = {"apple": 5, "banana": 3, "orange": 8}
-for x in Food:
-    print(x, Food[x])
+import time
+timer = input("Enter your time in seconds: ")
+for x in reversed(range(int(timer) + 1)):
+    seconds = int(x) % 60
+    minutes = (int(x) // 60) % 60
+    hours = (int(x) // 3600) 
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
