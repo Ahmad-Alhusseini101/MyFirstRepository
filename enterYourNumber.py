f688cae021668b0c026916dfdd325bad66379ab5
Ahmad Al-Hosseini <ahmadalhosseini101@gmail.com>
2026-09-17 15:28:56 +0800

    Matrices

--- enterYourNumber.py
+++
@@ -1,4 +0,0 @@
-Number = input("Enter a number between 1 and 10: ")
-while round(float(Number)) < 1 or round(float(Number)) > 10:
-    print("Invalid input. Please enter a number between 1 and 10.")
-    Number = input("Enter a number between 1 and 10")
