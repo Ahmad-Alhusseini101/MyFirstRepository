de50203d6f614b95d1bf6bdba241c5b906979bce
Ahmad Al-Hosseini <ahmadalhosseini101@gmail.com>
2026-09-17 15:19:19 +0800

    Shopping_Cart

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
 Food = []
+Price1 = 0
 while True:
     item = input("Enter a food you'd like to buy (press q to quit): ")
     if item.lower() == "q":
         break
     else: 
         Food.append(item)
-        price = round(float(input(f"Enter the price if a/an {Food}: ")))
-print(f"Here's your whole shopping list: {Food}, and the total price is: {price}")
+        price = round(float(input(f"Enter the price if a/an {item}: "))) + Price1
+print("Here's your whole shopping list: ", end="")
+for items in Food:
+    if Food[-1] == items:
+        print(f"and {items}", end=". ")
+    elif len(Food) > 2:
+        print(f"{items}", end=", ")
+    else: 
+        print(f"{items}", end=" ")
+print("Your total price is:", price)
