131d7208aa7231504be2f0958b3d4c294b896c9a
Ahmad Al-Hosseini <ahmadalhosseini101@gmail.com>
2026-09-24 15:17:22 +0800

    python variables

---
+++ pythonVariablesAndTracing.py
@@ -0,0 +1,9 @@
+school_name = "UWC Changshu China"
+def show_user_info():
+    global school_name 
+    user_name = "Ahmad"
+    user_age = 18
+    is_student = True
+    print(f"Hello, I'm {user_name}, I'm {user_age} years old, and if you're asking wether I'm a student or not, it's {is_student}, I'm a student learning at {school_name}")
+
+show_user_info()
