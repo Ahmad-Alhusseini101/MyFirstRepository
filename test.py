608862a4ec7ec7849c68c908c725481e3c30db5a
Ahmad Al-Hosseini <ahmadalhosseini101@gmail.com>
2026-09-24 15:07:58 +0800

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+#x = 9 
+
+#def increment_cost():
+ #   global x
+  #  print("test")
+   # x += 1
+    #print(f"{x} cost")
+
+#increment_cost()
+#print(f"{x} cost")
+something = "I like icecream"
+print(something[:-1])
+print(something[:])
